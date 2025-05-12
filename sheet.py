8827a36c55fdf01738256e03cb2f2cfa7b299c73
Nayan <nayanvaid.04@gmail.com>
2025-05-12 18:31:35 +0530

    improvements

--- sheet.py
+++ sheet.py
@@ -12,98 +12,83 @@ class PDF(FPDF):
 pdf = PDF()
 pdf.add_page()
 
+deg_list=[0,7.5,15,18,30,45,60,75,90]
+
 #Heading
 pdf.set_fill_color(144, 238, 144)
 pdf.cell(15,15,"Ratio",1,align="C",fill=True)
 pdf.set_fill_color(255, 182, 193)
-pdf.cell(19,15,"0°",1,align="C",fill=True)
-pdf.cell(19,15,"7.5°",1,align="C",fill=True)
-pdf.cell(19,15,"15°",1,align="C",fill=True)
-pdf.cell(19,15,"18°",1,align="C",fill=True)
-pdf.cell(19,15,"30°",1,align="C",fill=True)
-pdf.cell(19,15,"45°",1,align="C",fill=True)
-pdf.cell(19,15,"60°",1,align="C",fill=True)
-pdf.cell(19,15,"75°",1,align="C",fill=True)
-pdf.cell(19,15,"90°",1,align="C", ln=1,fill=True)
+
+for i in deg_list:
+    if i == 90:
+        pdf.cell(19,15,f"{i}°",1,align="C",fill=True,ln=1)
+    else:
+        pdf.cell(19,15,f"{i}°",1,align="C",fill=True)
 
 #sine
 pdf.set_fill_color(255, 255, 224)
 pdf.cell(15,15,"sin",1,align="C",fill=True)
 pdf.set_fill_color(173, 216, 230)
-pdf.cell(19,15,f"{math.sin(0)}",1,align="C",fill=True)
-pdf.cell(19,15,f"{round(math.sin(math.radians(7.5)),2)}",1,align="C",fill=True)
-pdf.cell(19,15,f"{round(math.sin(math.radians(15)),2)}",1,align="C",fill=True)
-pdf.cell(19,15,f"{round(math.sin(math.radians(18)),2)}",1,align="C",fill=True)
-pdf.cell(19,15,f"{round(math.sin(math.radians(30)),2)}",1,align="C",fill=True)
-pdf.cell(19,15,f"{round(math.sin(math.radians(45)),2)}",1,align="C",fill=True)
-pdf.cell(19,15,f"{round(math.sin(math.radians(60)),2)}",1,align="C",fill=True)
-pdf.cell(19,15,f"{round(math.sin(math.radians(75)),2)}",1,align="C",fill=True)
-pdf.cell(19,15,f"{round(math.sin(math.radians(90)),2)}",1,align="C", ln=1,fill=True)
+for i in deg_list:
+    if i == 90:
+        pdf.cell(19,15,f"{round(math.sin(math.radians(i)),2)}",1,align="C",fill=True,ln=1)
+    else:
+        pdf.cell(19,15,f"{round(math.sin(math.radians(i)),2)}",1,align="C",fill=True)
+
 #cosine
 pdf.set_fill_color(255, 255, 224)
 pdf.cell(15,15,"cos",1,align="C",fill=True)
 pdf.set_fill_color(173, 216, 230)
-pdf.cell(19,15,f"{math.cos(0)}",1,align="C",fill=True)
-pdf.cell(19,15,f"{round(math.cos(math.radians(7.5)),2)}",1,align="C",fill=True)
-pdf.cell(19,15,f"{round(math.cos(math.radians(15)),2)}",1,align="C",fill=True)
-pdf.cell(19,15,f"{round(math.cos(math.radians(18)),2)}",1,align="C",fill=True)
-pdf.cell(19,15,f"{round(math.cos(math.radians(30)),2)}",1,align="C",fill=True)
-pdf.cell(19,15,f"{round(math.cos(math.radians(45)),2)}",1,align="C",fill=True)
-pdf.cell(19,15,f"{round(math.cos(math.radians(60)),2)}",1,align="C",fill=True)
-pdf.cell(19,15,f"{round(math.cos(math.radians(75)),2)}",1,align="C",fill=True)
-pdf.cell(19,15,f"{round(math.cos(math.radians(90)),2)}",1,align="C", ln=1,fill=True)
+for i in deg_list:
+    if i == 90:
+        pdf.cell(19,15,f"{round(math.cos(math.radians(i)),2)}",1,align="C",fill=True,ln=1)
+    else:
+        pdf.cell(19,15,f"{round(math.cos(math.radians(i)),2)}",1,align="C",fill=True)
 #tan
 pdf.set_fill_color(255, 255, 224)
 pdf.cell(15,15,"tan",1,align="C",fill=True)
 pdf.set_fill_color(173, 216, 230)
-pdf.cell(19,15,f"{math.tan(0)}",1,align="C",fill=True)
-pdf.cell(19,15,f"{round(math.tan(math.radians(7.5)),2)}",1,align="C",fill=True)
-pdf.cell(19,15,f"{round(math.tan(math.radians(15)),2)}",1,align="C",fill=True)
-pdf.cell(19,15,f"{round(math.tan(math.radians(18)),2)}",1,align="C",fill=True)
-pdf.cell(19,15,f"{round(math.tan(math.radians(30)),2)}",1,align="C",fill=True)
-pdf.cell(19,15,f"{round(math.tan(math.radians(45)),2)}",1,align="C",fill=True)
-pdf.cell(19,15,f"{round(math.tan(math.radians(60)),2)}",1,align="C",fill=True)
-pdf.cell(19,15,f"{round(math.tan(math.radians(75)),2)}",1,align="C",fill=True)
-pdf.cell(19,15,"N.D.",1,align="C", ln=1,fill=True)
+for i in deg_list:
+    if i == 90:
+        pdf.cell(19,15,"N.D.",1,align="C", ln=1,fill=True)
+    else:
+        pdf.cell(19,15,f"{round(math.tan(math.radians(i)),2)}",1,align="C",fill=True)
+
 #cosec
 pdf.set_fill_color(255, 255, 224)
 pdf.cell(15,15,"cosec",1,align="C",fill=True)
 pdf.set_fill_color(173, 216, 230)
-pdf.cell(19,15,"N.D.",1,align="C",fill=True)
-pdf.cell(19,15,f"{round(1/(math.sin(math.radians(7.5))),2)}",1,align="C",fill=True)
-pdf.cell(19,15,f"{round(1/(math.sin(math.radians(15))),2)}",1,align="C",fill=True)
-pdf.cell(19,15,f"{round(1/(math.sin(math.radians(18))),2)}",1,align="C",fill=True)
-pdf.cell(19,15,f"{round(1/(math.sin(math.radians(30))),2)}",1,align="C",fill=True)
-pdf.cell(19,15,f"{round(1/(math.sin(math.radians(45))),2)}",1,align="C",fill=True)
-pdf.cell(19,15,f"{round(1/(math.sin(math.radians(60))),2)}",1,align="C",fill=True)
-pdf.cell(19,15,f"{round(1/(math.sin(math.radians(75))),2)}",1,align="C",fill=True)
-pdf.cell(19,15,f"{round(1/(math.sin(math.radians(90))),2)}",1,align="C", ln=1,fill=True)
+
+for i in deg_list:
+    if i == 0:
+        pdf.cell(19,15,"N.D.",1,align="C",fill=True)
+    elif i == 90:
+        pdf.cell(19,15,f"{round(1/(math.sin(math.radians(i))),2)}",1,align="C",ln=1,fill=True)
+    else:
+        pdf.cell(19,15,f"{round(1/(math.sin(math.radians(i))),2)}",1,align="C",fill=True)
+
 #sec
 pdf.set_fill_color(255, 255, 224)
 pdf.cell(15,15,"sec",1,align="C",fill=True)
 pdf.set_fill_color(173, 216, 230)
-pdf.cell(19,15,f"{round(1/(math.cos(math.radians(0))),2)}",1,align="C",fill=True)
-pdf.cell(19,15,f"{round(1/(math.cos(math.radians(7.5))),2)}",1,align="C",fill=True)
-pdf.cell(19,15,f"{round(1/(math.cos(math.radians(15))),2)}",1,align="C",fill=True)
-pdf.cell(19,15,f"{round(1/(math.cos(math.radians(18))),2)}",1,align="C",fill=True)
-pdf.cell(19,15,f"{round(1/(math.cos(math.radians(30))),2)}",1,align="C",fill=True)
-pdf.cell(19,15,f"{round(1/(math.cos(math.radians(45))),2)}",1,align="C",fill=True)
-pdf.cell(19,15,f"{round(1/(math.cos(math.radians(60))),2)}",1,align="C",fill=True)
-pdf.cell(19,15,f"{round(1/(math.cos(math.radians(75))),2)}",1,align="C",fill=True)
-pdf.cell(19,15,"N.D",1,align="C", ln=1,fill=True)
+for i in deg_list:
+    if i == 90:
+       pdf.cell(19,15,"N.D",1,align="C", ln=1,fill=True)
+    else:
+        pdf.cell(19,15,f"{round(1/(math.cos(math.radians(i))),2)}",1,align="C",fill=True)
+
+
 #cot
 pdf.set_fill_color(255, 255, 224)
 pdf.cell(15,15,"cot",1,align="C",fill=True)
 pdf.set_fill_color(173, 216, 230)
-pdf.cell(19,15,"N.D.",1,align="C",fill=True)
-pdf.cell(19,15,f"{round(1/(math.tan(math.radians(7.5))),2)}",1,align="C",fill=True)
-pdf.cell(19,15,f"{round(1/(math.tan(math.radians(15))),2)}",1,align="C",fill=True)
-pdf.cell(19,15,f"{round(1/(math.tan(math.radians(18))),2)}",1,align="C",fill=True)
-pdf.cell(19,15,f"{round(1/(math.tan(math.radians(30))),2)}",1,align="C",fill=True)
-pdf.cell(19,15,f"{round(1/(math.tan(math.radians(45))),2)}",1,align="C",fill=True)
-pdf.cell(19,15,f"{round(1/(math.tan(math.radians(60))),2)}",1,align="C",fill=True)
-pdf.cell(19,15,f"{round(1/(math.tan(math.radians(75))),2)}",1,align="C",fill=True)
-pdf.cell(19,15,f"{round(1/(math.tan(math.radians(90))),2)}",1,align="C", ln=1,fill=True)
+for i in deg_list:
+    if i == 0:
+        pdf.cell(19,15,"N.D.",1,align="C",fill=True)
+    elif i == 90:
+        pdf.cell(19,15,f"{round(1/(math.tan(math.radians(i))),2)}",1,align="C",ln=1,fill=True)
+    else:
+        pdf.cell(19,15,f"{round(1/(math.tan(math.radians(i))),2)}",1,align="C",fill=True)
 
 
 pdf.output("trigo_sheet.pdf")
